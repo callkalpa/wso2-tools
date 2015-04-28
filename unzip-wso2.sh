fec97a19c2a0ebc823175f782d7e16204b3b7f9f
Kalpa Welivitigoda <callkalpa@gmail.com>
2015-04-28 11:18:23 +0530

    adding unzip-wso2.sh

---
+++ unzip-wso2.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# usage: unzip-wso2.sh <product name and version without wso2 part> <target unzip location>
+ROOT_DIR="/home/kalpa/Resources/wso2"
+PRODUCT="$1"
+TARGET="$2"
+
+unzip "$ROOT_DIR/wso2$PRODUCT.zip" -d "$TARGET"
